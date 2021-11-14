bdafc6e71febfa720f717bf93e12208d9413a4a8
damon.brodie@gmail.com <damon.brodie@gmail.com>
2021-11-14 16:18:53 -0500

    Suppress Windows platform warnings.

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~F:BrodieTheatre.FormSettings.rkApp")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:BrodieTheatre.FormSettings.buttonSave_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:BrodieTheatre.FormSettings.FormSettings_Load(System.Object,System.EventArgs)")]
